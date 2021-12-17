05283cdf7457e3c4a089267483c5a412f1d3c6c5
R4vante <l.teegelbeckers@hotmail.com>
2021-12-17 19:51:23 +0100

    In my_subplot: Het maken van de light_beam met Michelson functie in de loop verplaatst, want bij elke itteratie moet nieuwe light_beam gemaakt worden. Hierdoor is til_reset() in Micheslon.py niet nodig.

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import matplotlib.pyplot as plt
 # MAIN
 def main():
     wavelength = 632.8e-9  # wavelength of HeNe laser
-    size = 30e-3  # size of the grid
+    size = 20e-3  # size of the grid
     N = 900  # number (NxN) of grid pixels
     R = 3e-3  # laser beam radius
     z1 = 8e-2  # length of arm 1
@@ -16,7 +16,7 @@ def main():
     Rbs = 0.5  # reflection beam splitter
     f = -100e-2  # focal length of positive lens
 
-    tx = 0.0e-3;ty = 0.0e-3  # tilt of mirror 1
+    tx = 2.0e-3;ty = 0.0e-3  # tilt of mirror 1
     tx_list = np.linspace(-2.0e-3, 2.0e-3, 5)
     ty_list = np.linspace(-2.0e-3, 2.0e-3, 5)
     grid_height = 2
@@ -30,34 +30,33 @@ def main():
         plt.show()
 
     def my_subplot(my_grid_width, my_grid_height, my_tx_list, my_ty_list):
-        light_beam = Michelson(wavelength, size, N, R, z1, z2, z3, z4, Rbs, f)
         i = 0
 
         # first figure
         fig1 = plt.figure()
 
         # subplots in x direction
+    
         for my_tx in my_tx_list:
             i += 1
+            light_beam = Michelson(wavelength, size, N, R, z1, z2, z3, z4, Rbs, f)
             light_beam.disturb(my_tx, 0)
             light_beam.interfere()
-            I = light_beam.get_intensity()
             light_beam.subplot_x(my_grid_width, my_grid_height, my_tx, args=i)
-            light_beam.reset_tilt(my_tx, 0)
+            
 
         # subplots in y direction
         for my_ty in my_ty_list:
             i += 1
+            light_beam = Michelson(wavelength, size, N, R, z1, z2, z3, z4, Rbs, f)
             light_beam.disturb(0, my_ty)
             light_beam.interfere()
             I = light_beam.get_intensity()
             light_beam.subplot_y(my_grid_width, my_grid_height, my_ty, args=i)
-            light_beam.reset_tilt(0, my_ty)
-        #plt.show()
 
-    # run_normal()
+    run_normal()
 
-    # Plot for tilt in x direction
+    #Plot for tilt in x direction
     # Create figure
     my_subplot(grid_width, grid_height, tx_list, ty_list)
 
